60aad6afe178c2454e6fb46019defab1c76b3200
Luke Horvat <lukehorvat@gmail.com>
2016-04-07 14:00:26 +1000

    FIX: Increase test timeout

--- test/index.js
+++ test/index.js
@@ -2,12 +2,14 @@ import should from "should";
 import gulp from "gulp";
 
 describe("gulpfile", () => {
-  it("should define gulp tasks", () => {
-    gulp.tasks.should.have.keys();
+  it("should define gulp tasks", function() {
+    this.timeout(5000); // Can take a few seconds to load all of the gulp tasks...
+
+    gulp.tasks.should.be.an.Object().and.have.keys();
 
     require("../lib");
 
-    gulp.tasks.should.have.keys(
+    gulp.tasks.should.be.an.Object().and.have.keys(
       "clean",
       "check-dependencies",
       "build-scripts",
